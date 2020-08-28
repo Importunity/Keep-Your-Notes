45c64e53ab06e4be5a51f4040b79036e3a011c56
Importunity <Amadeusherr@gmail.com>
2020-08-27 23:29:57 -0500

    Add icons to note

--- keep/src/components/Body/Main-Body/Note.jsx
+++ keep/src/components/Body/Main-Body/Note.jsx
@@ -5,11 +5,38 @@ function Note(props){
     return (
         <div className="note">
             <div className="card note-card">
-                <div className="card-header">
-                    <h1>{props.title}</h1>
+                <div className="card-header note-header">
+                    <span>
+                        <i class="fas fa-thumbtack thumb items"></i>
+                        <p>{props.title}</p>
+                    </span>
                 </div>
-                <div className="card-body">
+                <div className="card-body note-body">
                     <p>{props.content}</p>
+                </div>
+                <div className="card-footer note-footer">
+                    <div className="container">
+                        <div className="row items">
+                            <div className="col-md-2">
+                                <i class="fas fa-bell"></i>
+                            </div>
+                            <div className="col-md-2">
+                                <i class="fas fa-user-plus" title="collaborator"></i>
+                            </div>
+                            <div className="col-md-2">
+                                <i class="fas fa-palette"></i>
+                            </div>
+                            <div className="col-md-2">
+                                <i class="fas fa-image"></i>
+                            </div>
+                            <div className="col-md-2">
+                                <i className="fas fa-archive"></i>
+                            </div>
+                            <div className="col-md-2">
+                                <i class="fas fa-ellipsis-v"></i>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
