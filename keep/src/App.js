a2fd64c3ac8978d2bafbb3ce06f74ec1fd98cb19
Importunity <Amadeusherr@gmail.com>
2020-09-02 13:18:55 -0500

    Add providers

--- keep/src/App.js
+++ keep/src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Header/Navbar';
 import Frame from './components/Body/Frame';
+import Login from './components/Main/Login';
+
+// the provider helps us share states throughout the components
+import {Provider} from 'react-redux';
+import store from './store';
+
 function App() {
   return (
+    <Provider store={store}>
     <div className="header">
       <Navbar />
       <Frame />
     </div>
+    </Provider>
   );
 }
 
