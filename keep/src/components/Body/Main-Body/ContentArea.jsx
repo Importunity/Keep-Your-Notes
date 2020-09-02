893f46270ecd68940213e3081dc4146c6a8eeebc
Importunity <Amadeusherr@gmail.com>
2020-09-02 13:08:46 -0500

    Add proptype

--- keep/src/components/Body/Main-Body/ContentArea.jsx
+++ keep/src/components/Body/Main-Body/ContentArea.jsx
@@ -1,9 +1,27 @@
 import React, {useState} from 'react';
 import CreateNote from './CreateNote';
 import Note from './Note';
+import uuid from 'uuid';
+import {connect} from 'react-redux';
+import {getNotes} from '../../../actions/noteActions';
+import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 
-function ContentArea(){
+function ContentArea(props){
+    //console.log(props.note.notes);
+    useEffect(() =>{
+        props.getNotes();
+        //console.log(note.getNotes);
+    },[props.getNotes]);
+
+    const notess = props.note.notes;
+    //console.log(notess.length);
+    /*for(var i = 0; i < notess.length; i++){
+        console.log(notess[i]);
+    }*/
+
     const [notes, setNotes] = useState([]);
+
 
     // when adding a note, return the previous notes and the new note
     function addNote(newNote) {
@@ -13,6 +31,7 @@ function ContentArea(){
         });
     }
 
+
     // deletes the note on click of triple dots and clicking delete from dropdown
     function deleteNote(id) {
         setNotes(prevNotes => {
@@ -21,7 +40,6 @@ function ContentArea(){
             });
         });
     }
-        
 
     return (
         <div className="container">
@@ -31,8 +49,30 @@ function ContentArea(){
                     <Note key={index} id={index} title={note.title} content={note.content} onDelete={deleteNote} />);
                 })
             }
+            {notess.map((objectNote, index) => {
+                return (
+                    <Note key={index} id={index} title={objectNote.title} content={objectNote.content} />
+                );
+            })}
         </div>
     );
 }
 
-export default ContentArea;
+ContentArea.propTypes = {
+    getNotes: PropTypes.func.isRequired,
+    note: PropTypes.object.isRequired
+}
+
+/*function mapStateToProps(state){
+    return {
+        note: state.note
+    };
+}*/
+
+
+const mapStateToProps = state => ({
+    note: state.note
+});
+  
+
+export default connect(mapStateToProps, {getNotes})(ContentArea);
