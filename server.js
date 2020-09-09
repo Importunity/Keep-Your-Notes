db6c5401909277b818f64b00e5d80fffd9166364
Importunity <Amadeusherr@gmail.com>
2020-09-09 13:47:59 -0500

    Update server on routes

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
-import morgan from 'morgan';
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import path from 'path';
+const morgan = require('morgan');
+const express = require('express');
+const bodyParser =  require('body-parser');
+const cors = require('cors');
+const path = require('path');
 require('./config/config');
+const notes = require('./routes/api/notes.js');
 
 const app = express();
 
 // cors
-app.use(cors);
+//app.use(cors);
 // bodyparser
 app.use(bodyParser.json());
 //logger 
 app.use(morgan('dev'));
 
 // routes
-app.use('/api/users', userRoutes);
-app.use('/api/auth', authRoutes);
+//app.use('/api/users', userRoutes);
+//app.use('/api/auth', authRoutes);
+app.use('/api/notes', notes);
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
+
