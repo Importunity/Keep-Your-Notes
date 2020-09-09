5f79f7294101e6eb297d24381d83dc05d93c17d6
Importunity <Amadeusherr@gmail.com>
2020-09-09 13:53:50 -0500

    Add loading

--- keep/src/actions/noteActions.js
+++ keep/src/actions/noteActions.js
@@ -1,10 +1,21 @@
 // this javascript file is used to make request to the backend
-import {GET_NOTES, ADD_NOTE, DELETE_NOTE} from './types';
+import {GET_NOTES, ADD_NOTE, DELETE_NOTE, NOTES_LOADING} from './types';
+import axios from 'axios';
 
-export const getNotes = () =>{
+export const getNotes = () => dispatch =>{
     return {
         type: GET_NOTES
     }
+    /*dispatch(setNotesLoading());
+    axios
+    // getting the notes from the backend 
+        .get('/api/notes')
+        .then(response => dispatch({
+            // payload contains the data
+            type: GET_NOTES,
+            payload: response.data
+        }))*/
+    
 };
 
 export const deleteNote = (id) => {
@@ -18,5 +29,11 @@ export const addNote = (note) => {
     return {
         type: ADD_NOTE,
         payload: note
+    }
+};
+
+export const setNotesLoading = () => {
+    return {
+        type: NOTES_LOADING
     }
 };
