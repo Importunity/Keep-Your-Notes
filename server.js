e34c14a34513f37decfdfaa0a3dc0a9af740680c
Importunity <Amadeusherr@gmail.com>
2020-09-09 23:11:56 -0500

    Version 1.0

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const path = require('path');
 require('./config/config');
 const notes = require('./routes/api/notes.js');
+const { request } = require('http');
 
 const app = express();
 
@@ -20,6 +21,15 @@ app.use(morgan('dev'));
 //app.use('/api/auth', authRoutes);
 app.use('/api/notes', notes);
 
+// create build folder if in production i.e npm run build
+if(process.env.NODE_ENV == 'production'){
+    // loads the build folder 
+    app.use(express.static('keep/build'));
+    app.get('*', (request, response) => {
+        // directing to index.html
+        response.sendFile(path.resolve(__dirname, 'keep', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
