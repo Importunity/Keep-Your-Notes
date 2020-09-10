327f390dd81dfa5609bfd65de8b8a09dd810a39e
Importunity <Amadeusherr@gmail.com>
2020-09-10 15:38:55 -0500

    Add bfg to gitignore

---
+++ config/config.js
@@ -0,0 +1,20 @@
+// Import the mongoose module
+var mongoose = require('mongoose');
+var dotenv = require('dotenv');
+
+dotenv.config();
+
+// set up default mongoose connection 
+var mongoDB = process.env.MONGODB_URI;
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log("Database Connected"))
+    .catch(err => console.log(err));
+
+// get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+console.log(db.readyState);
+
+
