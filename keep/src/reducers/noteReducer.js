4f3926feed48cc49f046cfc474f901877e3de2c9
Importunity <Amadeusherr@gmail.com>
2020-09-09 22:42:14 -0500

    Add loading state

--- keep/src/reducers/noteReducer.js
+++ keep/src/reducers/noteReducer.js
@@ -1,31 +1,25 @@
 // where we check out actions for notes
-import uuid from 'uuid';
 import {GET_NOTES, ADD_NOTE, DELETE_NOTE, NOTES_LOADING} from '../actions/types';
 const initialState = {
-    notes: [
-        /*{id: uuid(), title: 'Hello', content: 'World'},
-        {id: uuid(), title: 'Hello1', content: 'World1'},
-        {id: uuid(), title: 'Hello2', content: 'World2'},
-        {id: uuid(), title: 'Hello3', content: 'World3'}*/
-
-    ],
+    notes: [],
     loading: false
 };
 
 export default function(state = initialState, action){
     switch(action.type){
-
         case GET_NOTES:
             return {
                 // returns all the objects of the state
-                ...state
+                ...state,
+                notes: action.payload,
+                loading: false
             };
         case DELETE_NOTE:
             // console.log(`payload is: ${action.payload}`);
             return {
                 ...state,
                 // action.payload contains the id
-                notes: state.notes.filter(note =>  note.id !== action.payload )
+                notes: state.notes.filter(note =>  note._id !== action.payload )
             };
         case ADD_NOTE:
             return {
