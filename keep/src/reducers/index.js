b992d3d8af8f8c0c01fbe9dcbff6c3a22d2a1b16
Amadeus Her <Amadeusherr@gmail.com>
2020-09-12 12:49:41 -0500

    add reducers to the main reducer file

--- keep/src/reducers/index.js
+++ keep/src/reducers/index.js
@@ -1,7 +1,11 @@
 // meeting place for all reducers
 import {combineReducers} from 'redux';
 import noteReducer from './noteReducer';
+import errorReducer from './errorReducer';
+import userReducer from './authReducer';
 
 export default combineReducers({
+    error: errorReducer,
+    user: userReducer,
     note: noteReducer
 });
