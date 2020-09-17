03435ed9be654842615539f3990b5cba86169d68
Importunity <Amadeusherr@gmail.com>
2020-09-17 13:34:48 -0500

    Add providers/ user loading

--- keep/src/App.js
+++ keep/src/App.js
@@ -1,27 +1,29 @@
 import React, { useEffect } from 'react';
 import './App.css';
-import Navbar from './components/Header/Navbar';
-import Frame from './components/Body/Frame';
 
-// the provider helps us share states throughout the components
+// main page
+import Main from './components/Main/Main';
+
 import {Provider} from 'react-redux';
 import store from './store';
-
-// load user
 import { loadUser } from './actions/authActions';
 
+
+
 function App() {
+  //console.log(store.dispatch(loadUser));
   useEffect(() => {
     store.dispatch(loadUser());
-  });
+  }, []);
+  
   return (
     <Provider store={store}>
-    <div className="header">
-      <Navbar />
-      <Frame />
+    <div>
+      <Main  />
     </div>
     </Provider>
   );
 }
 
 export default App;
+ 
